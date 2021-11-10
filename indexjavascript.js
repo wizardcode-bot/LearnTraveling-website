1294a846505b02c8e58062a1295daac6f5e64d02
Dark-ss <juancamilocalderon69@gmail.com>
2021-11-10 09:47:08 -0500

    coloque el slider en el index

---
+++ indexjavascript.js
@@ -0,0 +1,39 @@
+var imagenes = ['brasil.jpg','canada.jpg','japon.jpg','australia.jpg'],
+cont = 0;
+
+function carrousel(contenedor){
+
+    contenedor.addEventListener('click', e=>{
+        let atras = contenedor.querySelector('.atras'),
+        adelante = contenedor.querySelector('.adelante'),
+        img = contenedor.querySelector('img'),
+        targt = e.target;
+
+        if(targt == atras){
+            if(cont>0){
+                img.src = imagenes[cont - 1];
+                cont--;
+            }
+            else{
+                img.src = imagenes[imagenes.length -1];
+                cont = imagenes.length -1;
+            }
+
+        }else if(targt == adelante){
+            if(cont < imagenes.length -1){
+                img.src = imagenes[cont+1];
+                cont ++;
+            }
+            else{
+                img.src = imagenes[0];
+                cont= 0;
+            }
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    let contenedor = document.querySelector('.contenedor');
+    
+    carrousel(contenedor);
+});
