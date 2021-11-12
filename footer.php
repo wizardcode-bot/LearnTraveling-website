6c1873c496cf6fb0134b5598cbb71258b978c66a
Edinson <you@example.com>
2021-11-12 13:28:42 -0500

    arreglé el footer

--- footer.php
+++ footer.php
@@ -10,26 +10,32 @@
 </head>
 
 <footer>
-        <p class="contactar">
-            CONTACTANOS
-        </p>
+    <div class="contactos">
+        <p>CONTACTANOS</p>
+    </div>
+
+    <!-- contacto teléfono -->
+    <i class="fas fa-phone-alt telefono"> <p class="textotel">601-318-8844</p></i>
+    <div class="textofooter">
+        <p >LearnTraveling - Todos derechos reservados 2021 &copy;</p>
+    </div>
     
-        <i class="fas fa-phone-alt telefono"> <p class="textotel">601-318-8844</p></i>
-        <p class="textofooter">LearnTraveling - Todos derechos reservados 2021 &copy;</p>
-    
-            <div class="redes1">
-                <ul>
-                    <li><a href="https://github.com/wizardcode-bot/LearnTraveling-website"><i class="fab fa-github"></i></a></li>
-                    <li><a href="http://www.whatsapp.com"><i class="fab fa-whatsapp"></i></a></li>
-                    <li><a href="http://www.telegram.com"><i class="fab fa-telegram-plane"></i></a></li>
-                </ul>
-            </div>
-    
-            <div class="redes">
-                <ul>
-                    <li> <a href="http://www.facebook.com" class="facebook"><i class="fab fa-facebook-f"></i></a></li>
-                    <li> <a href="http://www.instagram.com" class="instagram"><i class="fab fa-instagram"></i></a></li>
-                    <li> <a href="http://www.twitter.com" class="twitter"><i class="fab fa-twitter"></i></a></li>
-                </ul>
-            </div>
-    </footer>
+    <!-- contácto de redes sociales -->
+    <div class="redes1">
+        <ul>
+            <li><a href="https://github.com/wizardcode-bot/LearnTraveling-website"><i class="fab fa-github"></i></a></li>
+            <li><a href="http://www.whatsapp.com"><i class="fab fa-whatsapp"></i></a></li>
+            <li><a href="http://www.telegram.com"><i class="fab fa-telegram-plane"></i></a></li>
+        </ul>
+    </div>
+        
+    <div class="redes">
+        <ul>
+            <li> <a href="http://www.facebook.com" class="facebook"><i class="fab fa-facebook-f"></i></a></li>
+            <li> <a href="http://www.instagram.com" class="instagram"><i class="fab fa-instagram"></i></a></li>
+            <li> <a href="http://www.twitter.com" class="twitter"><i class="fab fa-twitter"></i></a></li>
+        </ul>
+    </div>
+
+</footer>
+</html>
