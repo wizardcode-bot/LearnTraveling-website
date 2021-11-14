b4b23c016f77d386eeabd859ac29eb25313bc548
Dark-ss <juancamilocalderon69@gmail.com>
2021-11-14 18:00:24 -0500

    se modifico el help y el form

--- help.php
+++ help.php
@@ -2,9 +2,9 @@
 <html lang="en">
 
 <head>
-<?php
-include_once "nav.php";
-?>
+    <?php
+    include_once "nav.php";
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,7 +22,9 @@ include_once "nav.php";
         <button type="submit" id="problemsButton">Enviar</button>
     </div>
 
-    <a href="form.php" id="sign_up">¡Quiero registrarme!</a>
+    <button class="registrarse">
+        <a href="form.php" id="sign_up">¡Quiero registrarme!</a>
+    </button>
 
     <?
     include_once "registrar.php";
